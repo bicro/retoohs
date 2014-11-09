52b8f822f3ffb34274b7b985db1651eecd9fec1f
Rohit Swamy <rohitswamy@gmail.com>
2014-11-09 14:52:38 -0800

    added scene + images

---
+++ retoohs/retoohs/StartScene.swift
@@ -0,0 +1,18 @@
+//
+//  StartScene.swift
+//  retoohs
+//
+//  Created by Rohit Swamy on 11/9/14.
+//  Copyright (c) 2014 AlexRohit. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+import AVFoundation
+
+class StartScene: SKScene{
+    override func didMoveToView(view: SKView) {
+    }
+    
+    
+}
---
+++ retoohs/retoohs/StartScreen.swift
@@ -0,0 +1,9 @@
+//
+//  StartScreen.swift
+//  retoohs
+//
+//  Created by Rohit Swamy on 11/9/14.
+//  Copyright (c) 2014 AlexRohit. All rights reserved.
+//
+
+import Foundation
